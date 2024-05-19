a634a38b229b40e5d8428f8bb8bd382f62f74131
David <berpeda@alumni.uv.es>
2024-05-19 20:32:57 +0200

    consultas de POST aparcamiento, DELETE id aparcamiento realizadas

--- bicicletassql/src/main/java/com/proyectofinal/bicicletassql/controllers/AparcamientoController.java
+++ bicicletassql/src/main/java/com/proyectofinal/bicicletassql/controllers/AparcamientoController.java
@@ -3,27 +3,53 @@ package com.proyectofinal.bicicletassql.controllers;
 import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.slf4j.Logger;
 
 import com.proyectofinal.bicicletassql.models.Aparcamiento;
 import com.proyectofinal.bicicletassql.services.AparcamientoService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("")
 public class AparcamientoController {
     private final static Logger LOGGER = LoggerFactory.getLogger(AparcamientoController.class);
     @Autowired
-    private AparcamientoService aparcamientoService;
+    private AparcamientoService as;
 
     @GetMapping("/aparcamientos")
-    public List<Aparcamiento> findAparcamientos() {
+    public ResponseEntity<List<Aparcamiento>> findAparcamientos() {
         LOGGER.debug("View all aparcamientos");
-        return aparcamientoService.findAll();
+        return new ResponseEntity<>(as.findAll(), HttpStatus.OK);
     }
     
+    @PostMapping("/aparcamiento")
+    public ResponseEntity<?> create(@RequestBody Aparcamiento aparcamiento) throws IOException{
+        Aparcamiento a = as.create(aparcamiento);
+        if(a == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(a, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/aparcamiento/{id}")
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
+        Aparcamiento a = as.findById(id);
+        if(a == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        as.delete(a);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
--- bicicletassql/src/main/java/com/proyectofinal/bicicletassql/services/AparcamientoService.java
+++ bicicletassql/src/main/java/com/proyectofinal/bicicletassql/services/AparcamientoService.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.proyectofinal.bicicletassql.repositories.AparcamientoRepository;
 import com.proyectofinal.bicicletassql.models.Aparcamiento;
+
 import java.util.List;
 
 @Service
@@ -13,12 +14,26 @@ import java.util.List;
 public class AparcamientoService {
 
     @Autowired
-    private AparcamientoRepository aparcamientoRepository;
-    
+    private AparcamientoRepository ar;
+
     public List<Aparcamiento> findAll() {
-        return aparcamientoRepository.findAll();
+        return this.ar.findAll();
     }
 
-    
+    public Aparcamiento create(Aparcamiento aparcamiento) {
+        try {
+			Aparcamiento a = this.ar.save(aparcamiento);
+			return a;
+		} catch (IllegalArgumentException e) {
+			return null;
+		}
+    }
 
+    public void delete(Aparcamiento aparcamiento) {
+        this.ar.delete(aparcamiento);
+    }
+
+    public Aparcamiento findById(Integer id) {
+        return this.ar.findById(id).orElse(null);
+    }
 }
