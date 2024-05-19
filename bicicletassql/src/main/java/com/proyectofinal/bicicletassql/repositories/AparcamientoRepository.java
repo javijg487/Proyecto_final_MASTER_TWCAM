d3e57d3964f8cc6d0ffffc1c1a8a0f2d8bf72080
Javier Jiménez <javijg487@gmail.com>
2024-05-19 17:26:43 +0200

    repository arreglado

--- bicicletassql/src/main/java/com/proyectofinal/bicicletassql/repositories/AparcamientoRepository.java
+++ bicicletassql/src/main/java/com/proyectofinal/bicicletassql/repositories/AparcamientoRepository.java
@@ -3,6 +3,6 @@ import com.proyectofinal.bicicletassql.models.Aparcamiento;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AparcamientoRepository extends JpaRepository<Aparcamiento, String>{
+public interface AparcamientoRepository extends JpaRepository<Aparcamiento, Integer>{
     
 }
