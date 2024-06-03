d01a0d332854cae01a8acc3665ec5c0c2afef941
David <berpeda@alumni.uv.es>
2024-06-03 20:31:13 +0200

    Se ha introducido el swagger en bicicletas, bicicletassql, bicicletasmongo y autenticacion

--- autenticacion/src/main/java/com/proyectofinal/autenticacion/AutenticacionApplication.java
+++ autenticacion/src/main/java/com/proyectofinal/autenticacion/AutenticacionApplication.java
@@ -3,7 +3,26 @@ package com.proyectofinal.autenticacion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+  info =@Info(
+    title = "API de Autenticación",
+    version = "v1",
+    license = @License(
+      name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0"
+    ),
+    description = "API de Autenticación de Usuarios con JWT"
+  ),
+  servers = @Server(
+    url = "/",
+    description = "Production"
+  )
+)
 public class AutenticacionApplication {
 
 	public static void main(String[] args) {
--- bicicletasmongo/src/main/java/com/proyectofinal/bicicletasmongo/BicicletasmongoApplication.java
+++ bicicletasmongo/src/main/java/com/proyectofinal/bicicletasmongo/BicicletasmongoApplication.java
@@ -3,7 +3,26 @@ package com.proyectofinal.bicicletasmongo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+  info =@Info(
+    title = "Repositorio Bicicletas Mongo",
+    version = "v1",
+    license = @License(
+      name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0"
+    ),
+    description = "Repositorio de Bicicletas en Mongo"
+  ),
+  servers = @Server(
+    url = "/",
+    description = "Production"
+  )
+)
 public class BicicletasmongoApplication {
 
 	public static void main(String[] args) {
